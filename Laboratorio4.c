58159b2a20ffea29566c79a0c74de306a61dd15c
antorresca <65253336+antorresca@users.noreply.github.com>
2023-05-12 14:42:12 -0500

    Prueba3

--- Laboratorio4.c
+++ Laboratorio4.c
@@ -5,28 +5,5 @@
 #pragma config WDT=OFF
 
 void main(void){
-    ADCON1=15;
-    //Oa :D
-    TRISD=0;
     
-    TRISE=0;
-    //oa guapo
-    InicializaLCD();
-    
-    BorraLCD();
-    //MensajeLCD_Var("Hola mundo");
-    EscribeLCD_c('H');
-    EscribeLCD_c('o');
-    EscribeLCD_c('l');
-    EscribeLCD_c('a');
-    EscribeLCD_c(' ');
-    EscribeLCD_c('m');
-    EscribeLCD_c('u');
-    EscribeLCD_c('n');
-    EscribeLCD_c('d');
-    EscribeLCD_c('o');    
-    DireccionaLCD(0xC0);
-    EscribeLCD_n8(7,1);
-    while(1){
-    }    
 }
