c80f19887b850b08841701d6ee91da4f1e68af8c
antorresca <65253336+antorresca@users.noreply.github.com>
2023-05-12 14:35:11 -0500

    Principal

---
+++ Laboratorio4.c
@@ -0,0 +1,32 @@
+#include<xc.h>
+#define _XTAL_FREQ 1000000
+#include "LibLCDXC8.h"
+#pragma config FOSC=INTOSC_EC
+#pragma config WDT=OFF
+
+void main(void){
+    ADCON1=15;
+    
+    TRISD=0;
+    
+    TRISE=0;
+    
+    InicializaLCD();
+    
+    BorraLCD();
+    //MensajeLCD_Var("Hola mundo");
+    EscribeLCD_c('H');
+    EscribeLCD_c('o');
+    EscribeLCD_c('l');
+    EscribeLCD_c('a');
+    EscribeLCD_c(' ');
+    EscribeLCD_c('m');
+    EscribeLCD_c('u');
+    EscribeLCD_c('n');
+    EscribeLCD_c('d');
+    EscribeLCD_c('o');    
+    DireccionaLCD(0xC0);
+    EscribeLCD_n8(7,1);
+    while(1){
+    }    
+}
---
+++ build/default/production/Laboratorio4.p1.d
@@ -0,0 +1,4 @@
+ build/default/production/Laboratorio4.d  \
+ build/default/production/Laboratorio4.p1:  \
+ Laboratorio4.c  \
+LibLCDXC8.h 
